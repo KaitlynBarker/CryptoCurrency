20f4f042ece014114d25376500049cdc64900412
jfmarquezc <nanditovelvet@gmail.com>
2018-06-15 10:30:04 -0400

    Login changes

---
+++ CryptoCurrency/CryptoCurrency/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  CryptoCurrency
+//
+//  Created by José Fernando Márquez Cruz on 15/06/18.
+//  Copyright © 2018 Kaitlyn Barker. All rights reserved.
+//
+
+import UIKit
+
+let GREEN_THEME = UIColor(displayP3Red: 109/255, green: 201/255, blue: 149/255, alpha: 1)
+
