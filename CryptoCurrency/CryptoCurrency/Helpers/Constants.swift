657cbd2ed6c988fabc5d26ba01db1406cbbbb8a5
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MacBook-Pro.local>
2018-06-15 11:59:41 -0400

    create constants, pop currency TVC and cell, and currency files

---
+++ CryptoCurrency/CryptoCurrency/Helpers/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  CryptoCurrency
+//
+//  Created by Kaitlyn Barker on 6/15/18.
+//  Copyright © 2018 Kaitlyn Barker. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    static let shared = Constants()
+    
+    // api keys
+    
+    let baseURL = URL(string: "https://min-api.cryptocompare.com/data/")
+    let baseImageURL = URL(string: "https://www.cryptocompare.com/")
+    
+}
+
+// https://www.cryptocompare.com/ - imageURL - media/19633/btc.png
