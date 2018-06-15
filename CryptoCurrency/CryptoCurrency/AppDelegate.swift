77d69dc6b84b725935cb0564fea0cf0d9609e55b
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MacBook-Pro.local>
2018-06-15 11:01:00 -0400

    change everything back to beginning

--- CryptoCurrency/CryptoCurrency/AppDelegate.swift
+++ CryptoCurrency/CryptoCurrency/AppDelegate.swift
@@ -16,10 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-        let loginController = LoginViewController()
-        window?.rootViewController = loginController
         
         return true
     }
