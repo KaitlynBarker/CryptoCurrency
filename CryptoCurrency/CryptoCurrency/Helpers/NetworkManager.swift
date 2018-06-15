dff17a1271a42b9925d93000f9563503360d2d04
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MacBook-Pro.local>
2018-06-15 14:02:05 -0400

    create network manager file

---
+++ CryptoCurrency/CryptoCurrency/Helpers/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  CryptoCurrency
+//
+//  Created by Kaitlyn Barker on 6/15/18.
+//  Copyright © 2018 Kaitlyn Barker. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+}
