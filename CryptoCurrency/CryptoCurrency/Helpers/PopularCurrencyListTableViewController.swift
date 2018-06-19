6f1cdd1b38648756d70860e9240b9e4fbf290245
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MacBook-Pro.local>
2018-06-19 10:50:57 -0400

    remove unneccesary comments from code

--- CryptoCurrency/CryptoCurrency/Helpers/PopularCurrencyListTableViewController.swift
+++ CryptoCurrency/CryptoCurrency/Helpers/PopularCurrencyListTableViewController.swift
@@ -14,8 +14,6 @@ class PopularCurrencyListTableViewController: UITableViewController {
     
     // MARK: - Outlets
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,8 +30,6 @@ class PopularCurrencyListTableViewController: UITableViewController {
     
     // MARK: - Actions
     
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -54,41 +50,6 @@ class PopularCurrencyListTableViewController: UITableViewController {
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     // MARK: - Navigation
 
