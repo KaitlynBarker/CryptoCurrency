3fa04f69ce2f1fe42560bb9e4fe63c08b33105eb
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MacBook-Pro.local>
2018-06-22 13:54:33 -0400

    remove code that had to do with manual graph

--- CryptoCurrency/CryptoCurrency/Storyboards/CurrencyDetailViewController.swift
+++ CryptoCurrency/CryptoCurrency/Storyboards/CurrencyDetailViewController.swift
@@ -12,7 +12,6 @@ class CurrencyDetailViewController: UIViewController {
     
     // MARK: - Outlets
     
-    @IBOutlet weak var graphView: GraphView!
     @IBOutlet weak var currencyNameLabel: UILabel!
     @IBOutlet weak var conversionLabel: UILabel!
     @IBOutlet weak var currentPriceLabel: UILabel!
@@ -24,13 +23,6 @@ class CurrencyDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.graphView.backgroundColor = UIColor.darkGray
-//        self.graphView.lineColor = UIColor.customGreen
-        self.graphView.circleColor = UIColor.clear
-        self.graphView.axisColor = UIColor.white
-        self.graphView.deltaX = 2
-        self.graphView.deltaY = 1000
         
         self.populateView()
     }
@@ -52,29 +44,15 @@ class CurrencyDetailViewController: UIViewController {
             var points: [CGPoint] = []
 
             for exchange in exchanges {
-                let price = Int(exchange.price) // vertical
+                let price = Int(exchange.price)
                 let robotTime = exchange.lastupdate
-                let hour = Double(robotTime).getDateHour() // horizontal
-//                let day = Double(robotTime).getDateDay()
+                let hour = Double(robotTime).getDateHour()
                 let currentPoint = CGPoint(x: hour, y: price)
                 points.append(currentPoint)
             }
             
-//            let f: (CGFloat) -> CGPoint = {
-//                let noiseY = (CGFloat(arc4random_uniform(2)) * 2 - 1) * CGFloat(arc4random_uniform(4))
-//                let noiseX = (CGFloat(arc4random_uniform(2)) * 2 - 1) * CGFloat(arc4random_uniform(4))
-//                let b: CGFloat = 5
-//                let y = 2 * $0 + b + noiseY
-//
-//                return CGPoint(x: $0 + noiseY, y: y)
-//            }
-//
-//            let xPoints = [Int](1..<20)
-//            let points = xPoints.map { f(CGFloat($0 * 10)) }
-            
             
             DispatchQueue.main.async {
-                self.graphView.plot(points)
                 self.currencyNameLabel.text = "Name: \(currencyInfo.fullName)"
                 self.conversionLabel.text = "\(currencyInfo.name) -> USD"
                 self.currentPriceLabel.text = "Current Price: \(aggregatedData.price)"
@@ -84,16 +62,4 @@ class CurrencyDetailViewController: UIViewController {
             }
         }
     }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
